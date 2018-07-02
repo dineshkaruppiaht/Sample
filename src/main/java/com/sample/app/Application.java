0653f5f54d40173dae8bd2611ed7f33730d9e796
dinesh.k <dinesh.k@SOLVERLAPT110.SOLVER.SVMDOMAIN>
2018-07-02 11:13:35 +0530

    test1

--- src/main/java/com/sample/app/Application.java
+++ src/main/java/com/sample/app/Application.java
@@ -19,10 +19,6 @@ import org.springframework.web.bind.annotation.RestController;
 		DataSourceTransactionManagerAutoConfiguration.class, HibernateJpaAutoConfiguration.class })
 public class Application extends SpringBootServletInitializer {
 	
-//	@Override
-//    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-//        return application.sources(Application.class);
-//    }
 	
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
